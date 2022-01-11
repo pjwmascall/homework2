9ae63d7e6bec0d18e0a5de3ddd1f6e60d759c915
Paul Mascall <pjwmascall@hotmail.com>
2022-01-11 19:59:52 +0000

    completed all homework

---
+++ exercise_b_users.py
@@ -0,0 +1,82 @@
+users = {
+  "Jonathan": {
+    "twitter": "jonnyt",
+    "lottery_numbers": [6, 12, 49, 33, 45, 20],
+    "home_town": "Stirling",
+    "pets": [
+    {
+      "name": "fluffy",
+      "species": "cat"
+    },
+    {
+      "name": "fido",
+      "species": "dog"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    }
+  ]
+  },
+  "Erik": {
+    "twitter": "eriksf",
+    "lottery_numbers": [18, 34, 8, 11, 24],
+    "home_town": "Linlithgow",
+    "pets": [
+    {
+      "name": "nemo",
+      "species": "fish"
+    },
+    {
+      "name": "kevin",
+      "species": "fish"
+    },
+    {
+      "name": "spike",
+      "species": "dog"
+    },
+    {
+      "name": "rupert",
+      "species": "parrot"
+    }
+  ]
+  },
+  "Avril": {
+    "twitter": "bridgpally",
+    "lottery_numbers": [12, 14, 33, 38, 9, 25],
+    "home_town": "Dunbar",
+    "pets": [
+      {
+        "name": "monty",
+        "species": "snake"
+      }
+    ]
+  }
+}
+
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users["Jonathan"]["twitter"])
+# 2. Get Erik's hometown
+print(users["Erik"]["home_town"])
+# 3. Get the list of Erik's lottery numbers
+print(users["Erik"]["lottery_numbers"])
+# 4. Get the species of Avril's pet Monty
+for pet in users["Avril"]["pets"]:
+  if (pet["name"].capitalize() == "Monty") : print(pet["species"]) 
+# 5. Get the smallest of Erik's lottery numbers
+print(min(users["Erik"]["lottery_numbers"]))
+# 6. Return an list of Avril's lottery numbers that are even
+evenlist = [num for num in users["Avril"]["lottery_numbers"] if num % 2 == 0]
+# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"]["lottery_numbers"].append(7)
+# 8. Change Erik's hometown to Edinburgh
+users["Erik"]["home_town"] = "Edinburgh"
+# 9. Add a pet dog to Erik called "fluffy"
+users["Erik"]["pets"].append({"name" : "fluffy", "species" : "dog"})
+# 10. Add another person to the users dictionary
+users["Jacob"] = {
+  "twitter" : "jhpotter",
+  "lottery_numbers" : [1, 2, 3, 4, 5],
+  "home_town" : "London",
+  "pets" : []
+}
